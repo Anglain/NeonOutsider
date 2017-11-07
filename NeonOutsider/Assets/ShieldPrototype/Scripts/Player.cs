e4bfa72f55f6c97129783fdea214a899ca568671
Taras Martynyuk <tarasmartynyuk22@gmail.com>
2017-11-07 02:44:49 +0200

    added hack jump to player, shield with proper collider

--- NeonOutsider/Assets/ShieldPrototype/Scripts/Player.cs
+++ NeonOutsider/Assets/ShieldPrototype/Scripts/Player.cs
@@ -4,18 +4,52 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-	public float speed = 3f;
+	public float speed = 100f;
+	public float jumpFactor = 1f;
+	int framesToJumpAgain = 10;
+	int framesPast = 0;
+	Rigidbody2D rb;
+	bool jumping = false;
 	// Use this for initialization
 	void Start () 
 	{
-		
+		rb = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
 		// Debug.Lo
-		transform.position += Vector3.right * Input.GetAxis("Horizontal") * speed * Time.deltaTime;
-		
+		// transform.position += Vector3.right * Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+	}
+
+	void FixedUpdate()
+	{
+		// Vector2.right * Input.GetAxis("Horizontal") * speed * Time.deltaTime
+		// rb.MovePosition(rb.position);
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		Vector2 moveVec = new Vector2(moveHorizontal * speed * Time.deltaTime, 0.0f);
+		rb.AddForce(moveVec);
+
+
+
+		float moveJump = Input.GetAxisRaw("Jump");
+		if(moveJump != 0.0f && jumping == false)
+		{
+			jumping = true;
+			// Vector3 jumpVector = new Vector3(0.0f, 1.0f , 0.0f);
+			Vector3 jumpVector = Vector3.up;
+			rb.AddForce(jumpVector * jumpFactor , ForceMode2D.Impulse);
+		}
+		if(jumping)
+		{
+			framesPast++;
+			if(framesPast >= framesToJumpAgain)
+			{
+				jumping = false;
+				framesPast = 0;
+			}
+		}
+
 	}
 }
