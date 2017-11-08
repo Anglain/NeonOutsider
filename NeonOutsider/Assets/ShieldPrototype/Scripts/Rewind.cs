82b8f976910a2b4b1581b797830df8683b0f8120
Taras Martynyuk <tarasmartynyuk22@gmail.com>
2017-11-09 00:13:05 +0200

    Added Rewind classes, decided on Shield Script interacting only with  it's prefab

---
+++ NeonOutsider/Assets/ShieldPrototype/Scripts/Rewind.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// keeps track of recently used abilities,
+/// supplies methods for player to be able to use the skill again
+/// </summary>
+public class RewindController : Singleton<RewindController>
+{
+	private int maxUsageEntries = 3;
+	private LinkedList<SkillUsage> skillMemory;
+
+	public void DuplicateLastSkill()
+	{
+		SkillUsage lastSkill = skillMemory.First.Value;
+		skillMemory.RemoveFirst();
+
+		switch(lastSkill.skillType)
+		{
+			case Skill.Shield:
+			case Skill.Wall:
+			{
+
+				break;
+			}
+
+			case Skill.Gun:
+			{
+				break;
+			}
+		}
+	}
+	
+	public void RememberSkillUsage(SkillUsage usage)
+	{
+		int skillsRemembered = skillMemory.Count;
+		skillMemory.AddFirst(usage);
+
+		if(skillsRemembered >= maxUsageEntries)
+		{
+			skillMemory.RemoveLast();
+		}
+	}
+}
+
+/// <summary>
+/// Keeps all data about skill usage 
+/// </summary>
+public struct SkillUsage
+{
+	// public readonly Vector3 position;
+	public readonly GameObject gameObject;
+	public readonly Skill skillType;
+
+	/// <summary>
+	/// pass a prefab of wall or shield when they are deactivated
+	/// or null otherwise
+	/// </summary>
+	/// <param name="gameObject"></param>
+	/// <param name="skillType"></param>
+	public SkillUsage(GameObject gameObject, Skill skillType)
+	{
+		this.gameObject = gameObject;
+		this.skillType = skillType;
+	}
+}
+
+public enum Skill
+{
+	Shield, Wall, Gun
+}
--- NeonOutsider/Assets/ShieldPrototype/Scripts/Shield.cs
+++ NeonOutsider/Assets/ShieldPrototype/Scripts/Shield.cs
@@ -1,47 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 /// <summary>
-/// Component that has all the shield ability logic
-/// Is added to the player gameobject
-/// When time is rewinded, the additional shield will be spawned through this class
+/// Added to the shield prefab
 /// </summary>
 public class Shield : MonoBehaviour
 {
-	// it will be our timer that will indicate where the shield was last used (for rewinding)
-	public GameObject rememberedShieldPosition;
-	public GameObject playersShield;	
-	public GameObject shieldPrefab;
-	float shieldDuration = 3f;
-	float timeSinceShieldTurnedOn = 0f;
-	bool shieldActive = false;
+	public bool wasRemembered;
+	
+	private float timeSinceShieldTurnedOn= 0;
+	private const float shieldDuration = 3f;
+	
+	void Start()
+	{
+		 
+	}
 
-	/// <summary>
-	/// turns shield on upon button press,
-	/// places a gameobject label at the start point,
-	/// keeps the shield on for duration, turns it off afterwards
-	/// </summary>
 	void Update()
 	{
-		if(Input.GetKeyDown(KeyCode.F))
-		{	// turn shield on
-			playersShield.SetActive(true);
-			shieldActive = true;
-			// move timer icon to be used by rewind skill
-			rememberedShieldPosition.transform.position = playersShield.transform.position;
-			rememberedShieldPosition.SetActive(true);
-		}
-		if(shieldActive)
+		timeSinceShieldTurnedOn += Time.deltaTime;
+		if(timeSinceShieldTurnedOn >= shieldDuration)
 		{
-			timeSinceShieldTurnedOn += Time.deltaTime;
-			if(timeSinceShieldTurnedOn >= shieldDuration)
+			this.gameObject.SetActive(false);
+			if(wasRemembered == false)		// add to stack if this is the first usage of skill
 			{
-				playersShield.SetActive(false);
-				shieldActive = false;
-				timeSinceShieldTurnedOn = 0;
+				SkillUsage usage = new SkillUsage(this.gameObject, Skill.Shield);
+				
 			}
 		}
 	}
-
 }
